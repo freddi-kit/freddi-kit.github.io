0414fe9785db9c51c0790910376321af1c090ca1
freddi-kit <never_end_journey@hotmail.com>
2018-03-31 12:54:55 +0900

    タイトルをつけてみた。画面の大きさでページのキャンバスサイズも変わるようにした

--- js/toppage_canvas.js
+++ js/toppage_canvas.js
@@ -1,6 +1,17 @@
 var cnv;
+var title_image;
 
-function setupWindow() { resizeCanvas(windowWidth / 2, windowHeight); }
+function setupWindow() {
+  var winSetWidth, winSetHeight;
+  winSetWidth = (windowWidth / 2 > 200) ? windowWidth / 2 : windowWidth;
+
+  winSetHeight =
+      max((windowWidth / 2 > 200) ? windowHeight : windowHeight / 2, 500);
+
+  resizeCanvas(winSetWidth, winSetHeight);
+}
+
+function preload() { title_image = loadImage("./media/title.png"); }
 
 function setup() {
   cnv = createCanvas(windowWidth / 2, windowHeight);
@@ -14,6 +25,11 @@ function draw() {
     background(41, 41, 41);
   else
     background(60, 210, 210);
+
+  imageMode(CENTER);
+  title_width = min(title_image.width, width - 50)
+  title_heigth = title_image.height * (title_width / title_image.width)
+  image(title_image, width / 2, height / 2, title_width, title_heigth)
 }
 
 function windowResized() { setupWindow(); }
