ccd04c5d63d386b4ec48ae8a617b0aee9e0d9127
freddi-kit <never_end_journey@hotmail.com>
2018-03-31 13:07:37 +0900

    タイトルに効果を追加。gimpファイルを消した

--- js/toppage_canvas.js
+++ js/toppage_canvas.js
@@ -1,5 +1,6 @@
 var cnv;
 var title_image;
+var in_counter;
 
 function setupWindow() {
   var winSetWidth, winSetHeight;
@@ -11,25 +12,35 @@ function setupWindow() {
   resizeCanvas(winSetWidth, winSetHeight);
 }
 
-function preload() { title_image = loadImage("./media/title.png"); }
+function preload() {
+  title_image = loadImage("./media/title.png");
+  in_counter = 1;
+}
 
 function setup() {
   cnv = createCanvas(windowWidth / 2, windowHeight);
   cnv.parent("toppage_canvas");
   setupWindow();
   noSmooth();
+  imageMode(CENTER);
 }
 
 function draw() {
+  // 昼夜の画面の明るさ設定
   if (hour() >= 20 || hour() <= 4)
     background(41, 41, 41);
   else
     background(60, 210, 210);
 
-  imageMode(CENTER);
+  // タイトルの大きさ、画面に合わせる
   title_width = min(title_image.width, width - 50)
   title_heigth = title_image.height * (title_width / title_image.width)
+
+  // タイトルがじわーっとなるやつ
+  if (in_counter < 255) in_counter += 5;
+  tint(255, min(255, in_counter));
   image(title_image, width / 2, height / 2, title_width, title_heigth)
+  tint(255, 255); // ココでtintをもとに戻さないと他に影響が出る
 }
 
 function windowResized() { setupWindow(); }
